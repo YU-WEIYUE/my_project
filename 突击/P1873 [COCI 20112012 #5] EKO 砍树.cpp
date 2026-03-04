d4cc43cc74c4ed83f3683b1a87d800a92a16708c
Rain <368446949@qq.com>
2026-03-04 16:30:31 +0800

    我服了为什么j++一定要放外面

---
+++ 突击/P1873 [COCI 20112012 #5] EKO 砍树.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+long long n,temp=0;
+long tree[1000001];
+long long m;
+int main(){
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)cin>>tree[i];
+    sort(tree+1,tree+n+1);
+    long long j=n;
+    while(temp<m){
+        temp+=(tree[j]-tree[j-1])*(n-j+1);
+        j--;
+    }
+    long long ans=tree[j]+(temp-m)/(n-j);
+    cout<<ans;
+    return 0;
+}            
