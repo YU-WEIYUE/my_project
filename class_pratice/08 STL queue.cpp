1f709749289497771220fb8e5ce2b8ce9274f929
Rain <368446949@qq.com>
2026-03-07 23:56:32 +0800

    霉好的一天

---
+++ class_pratice/08 STL queue.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    queue<int>q;
+    int n,m;cin>>n>>m;
+    for(int i=1;i<=n;i++)q.push(i);
+    while(!q.empty()){
+        for(int i=1;i<m;i++){
+            q.push(q.back());//把队首元素复制到队尾
+            q.pop();//删除队首元素
+        }
+        cout<<q.front()<<endl;
+        q.pop();
+    }
+    cout<<endl;
+    return 0;
+}
